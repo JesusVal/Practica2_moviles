4b5b78688c310ca55f9eb558139285a1338d3e18
JesusVal <kzkz98@outlook.com>
2020-10-19 23:18:24 -0500

    Hive added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pract_dos/home/home_page.dart';
 
-void main() {
+void main() async {
   // TODO: inicializar hive y agregar el adapter
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDocDir.path);
+
   runApp(MyApp());
 }
 
